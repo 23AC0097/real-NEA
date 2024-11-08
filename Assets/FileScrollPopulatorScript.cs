d59c8821dfe2851c24f7b8af641911eea52a5345
Alice Clarke <23ac0097@barton.ac.uk>
2024-11-08 09:58:50 +0000

    Added files

---
+++ Assets/FileScrollPopulatorScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows;
+
+public class FileScrollPopulatorScript : MonoBehaviour
+{
+    public GameObject FilePanel;
+    public RectTransform rectTransform;
+    
+
+    private void Awake()
+    {
+        string path = @"C:\projects\NEAProj\Assets\CreatureData";
+        rectTransform = GetComponent<ScrollRect>().content;
+        DirectoryInfo dirInfo = new DirectoryInfo(path);
+        FileInfo[] fileInfo = dirInfo.GetFiles("?.txt");
+        foreach (FileInfo file in fileInfo)
+        {
+            Debug.Log("Making a File Scroll Thing");
+            GameObject instantiatedPanel = Instantiate(FilePanel);
+            instantiatedPanel.transform.parent = rectTransform;
+        }
+    }
+}
